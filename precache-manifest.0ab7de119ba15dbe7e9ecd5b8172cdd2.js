cf442a303bdb305c559f920fccbcec1a46486823
Philippe <philippe@five.team>
2021-03-27 18:18:42 +0100

    Updates

---
+++ precache-manifest.0ab7de119ba15dbe7e9ecd5b8172cdd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8023ac00b0ca964c283919ed53f2b3",
+    "url": "/dapp-votes/index.html"
+  },
+  {
+    "revision": "3674ec41f6b735fe8810",
+    "url": "/dapp-votes/static/css/2.721072e4.chunk.css"
+  },
+  {
+    "revision": "cf77e87262c7c4b1761e",
+    "url": "/dapp-votes/static/css/main.b806a580.chunk.css"
+  },
+  {
+    "revision": "3674ec41f6b735fe8810",
+    "url": "/dapp-votes/static/js/2.9ff72476.chunk.js"
+  },
+  {
+    "revision": "cf77e87262c7c4b1761e",
+    "url": "/dapp-votes/static/js/main.83380098.chunk.js"
+  },
+  {
+    "revision": "9f3722348f4353e78266",
+    "url": "/dapp-votes/static/js/runtime-main.46f9f716.js"
+  }
+]);
